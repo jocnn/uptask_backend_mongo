cb7dad497a7ad3af5cc45ad0d75362d53530c737
jocnn <ijann_@outlook.com>
2022-05-23 13:12:19 -0300

    evitando registro duplicado con email

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,6 +2,17 @@ import Usuario from "../models/usuarioModel.js"
 
 const registrarUsuario = async (req, res) => {
   
+  const { email } = req.body
+  const existeUsuario = await Usuario.findOne({ email })
+  
+  /**
+   * Evitando duplicado de email
+   */
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado")
+    return res.status(400).json({ msj: error.message })
+  }
+
   try {
     const usuario = new Usuario(req.body)
     // console.info(usuario)
