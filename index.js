5895aa6248edfecd169a0674f6a5b5b73a0fb9fe
jocnn <ijann_@outlook.com>
2022-05-23 09:39:44 -0300

    definiendo la ruta de usuario en index

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from 'express'
 import 'dotenv/config'
 
 import connectDB from './config/db.js'
+import usuarioRouter from './routes/usuarioRoutes.js'
 
 const app = express()
 
 connectDB()
 
-app.listen(process.env.PORT_SERVER, () => {
-  console.log(`Servidor corriendo en el puerto ${process.env.PORT_SERVER}`)
+/**
+ * ROUTING
+ */
+app.use('/api/usuarios', usuarioRouter)
+
+
+const PORT_SERV = process.env.PORT_SERVER || 4000
+
+app.listen(PORT_SERV, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT_SERV}`)
 })
