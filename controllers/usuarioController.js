1d45973517f8aa68d82d3b53becb3ad186b11ceb
jocnn <ijann_@outlook.com>
2022-05-23 13:04:45 -0300

    escribiendo en la db

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,18 @@
-const registrarUsuario = (req, res) => {
-  res.json({ msj: "desde api"})
+import Usuario from "../models/usuarioModel.js"
+
+const registrarUsuario = async (req, res) => {
+  
+  try {
+    const usuario = new Usuario(req.body)
+    // console.info(usuario)
+
+    const usuarioAlmacenado = await usuario.save()
+    res.json(usuarioAlmacenado)
+
+  } catch (error) {
+    console.error(error)
+  }
+  
 }
 
 export {
