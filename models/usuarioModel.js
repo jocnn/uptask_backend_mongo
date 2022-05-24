65b4a6a919e25d07b2441dcf56c75e1b94e7a68d
jocnn <ijann_@outlook.com>
2022-05-24 16:38:28 -0300

    omisión del hash del password si no es modificada

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -30,6 +30,9 @@ const usuarioSchema = mongoose.Schema({
 })
 
 usuarioSchema.pre('save', async function(next) {
+  if (!this.isModified("password")) {
+    next()
+  }
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
