8f5a243a4b045946dfaea7c6ccff47d5b02ff81e
jocnn <ijann_@outlook.com>
2022-05-24 19:21:42 -0300

    validando usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -29,7 +29,24 @@ const registrarUsuario = async (req, res) => {
 }
 
 const autenticarUsuario = async (req, res) => {
+  const { email, password } = req.body
+
+  // comprobar si esta registrado
+  const usuario = await Usuario.findOne({email})
+  console.info(usuario)
+  if (!usuario) {
+    const error = new Error("El usuario no existe")
+    return res.status(404).json({ msg: error.message })
+  }
   
+  // comprobar si confirmo el email
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada")
+    return res.status(403).json({ msg: error.message })
+  }
+
+  // comprobar el password
+
 }
 
 export {
