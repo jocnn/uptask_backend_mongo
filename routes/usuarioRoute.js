dd75fd2e669928f6f92dc93825d93e3f5564f9c3
jocnn <ijann_@outlook.com>
2022-05-25 10:41:52 -0300

    ruta de confirmación de cuenta

--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registrarUsuario, autenticarUsuario } from "../controllers/usuarioController.js";
+import { registrarUsuario, autenticarUsuario, confirmar } from "../controllers/usuarioController.js";
 
 const router = express.Router()
 
 router.post('/', registrarUsuario)
 router.post('/login', autenticarUsuario)
+router.get('/confirmar/:token', confirmar)
 
 export default router
