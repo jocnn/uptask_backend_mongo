6b6842cf38dce140690c7c8bc27872f5ed49f0a6
jocnn <ijann_@outlook.com>
2022-05-26 12:51:36 -0300

    definiendo los metodos del controlador de proyecto

---
+++ controllers/ProyectoController.js
@@ -0,0 +1,28 @@
+
+
+const obtenerProyectos = async (req, res) => {}
+
+const obtenerProyecto = async (req, res) => {}
+
+const nuevoProyecto = async (req, res) => {}
+
+const editarProyecto = async (req, res) => {}
+
+const eliminarProyecto = async (req, res) => {}
+
+const agregarColaboradorProyecto = async (req, res) => {}
+
+const eliminarColaboradorProyecto = async (req, res) => {}
+
+const obtenerTareasProyecto = async (req, res) => {}
+
+export {
+  obtenerProyectos,
+  obtenerProyecto,
+  nuevoProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaboradorProyecto,
+  eliminarColaboradorProyecto,
+  obtenerTareasProyecto
+}
