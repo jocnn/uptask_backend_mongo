96077bb14e65aafed57660c8f0b6ab1908f08134
jocnn <ijann_@outlook.com>
2022-05-26 20:43:37 -0300

    definiendo controlador para crear nuevos proyectos

--- controllers/ProyectoController.js
+++ controllers/ProyectoController.js
@@ -1,10 +1,20 @@
-
+import Proyecto from "../models/ProyectoModel.js"
 
 const obtenerProyectos = async (req, res) => {}
 
 const obtenerProyecto = async (req, res) => {}
 
-const nuevoProyecto = async (req, res) => {}
+const nuevoProyecto = async (req, res) => {
+  const proyecto = new Proyecto(req.body)
+  proyecto.creador = req.usuario._id
+
+  try {
+    const proyectoAlmacenado = await proyecto.save()
+    res.json(proyectoAlmacenado)
+  } catch (error) {
+    console.log("🚀 ~ file: ProyectoController.js ~ line 14 ~ nuevoProyecto ~ error", error)
+  }
+}
 
 const editarProyecto = async (req, res) => {}
 
