dcf1d6dd73f29e5d5e2b47fbd7c1b47bcb2bce1f
jocnn <ijann_@outlook.com>
2022-05-27 18:59:35 -0300

    definiendo el método de actualizar tarea

--- controllers/TareaController.js
+++ controllers/TareaController.js
@@ -41,7 +41,32 @@ const obtenerTarea = async (req, res) => {
 
 }
 
-const actualizarTarea = async (req, res) => {}
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params
+  const tarea = await Tarea.findById(id).populate("proyecto")
+  
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada")
+    return res.status(404).json({ msg: error.message })
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no válida")
+    return res.status(403).json({ msg: error.message })
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre
+  tarea.descripcion = req.body.descripcion || tarea.descripcion
+  tarea.prioridad = req.body.prioridad || tarea.prioridad
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega
+
+  try {
+    const tareaAlmacenada = await tarea.save()
+    res.json(tareaAlmacenada)
+  } catch (error) {
+    console.log("🚀 ~ file: TareaController.js ~ line 66 ~ actualizarTarea ~ error", error)
+  }
+}
 
 const eliminarTarea = async (req, res) => {}
 
